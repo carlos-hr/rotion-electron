fa1c272c91855d965036589b7b0c5ff43214ae76
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-12-01 22:38:51 -0300

    feat: eslint and ts config

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,11 @@
-import "./styles/global.css";
+import './styles/global.css'
 
-export function App(): JSX.Element {
+export function App() {
   return (
     <div>
-      <h1 className="text-violet-400" style={{ fontFamily: "Inter" }}>
+      <h1 className="text-violet-400" style={{ fontFamily: 'Inter' }}>
         e aee
       </h1>
     </div>
-  );
+  )
 }
